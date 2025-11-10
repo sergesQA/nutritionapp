4ac00f265c393d5a6c0b6f757c40b2fe1598bb7a
sergesQA <98586019+sergesQA@users.noreply.github.com>
2025-11-10 14:09:22 +0200

    Update cognito-config.ts

--- lib/cognito-config.ts
+++ lib/cognito-config.ts
@@ -1,10 +1,10 @@
 export const cognito = {
   clientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || "2r72lfm59rs6257glg6gt7sfa9",
-  clientSecret: process.env.COGNITO_CLIENT_SECRET || "<client secret>",
+  clientSecret: process.env.COGNITO_CLIENT_SECRET || "2446k8gchpuctd11spq14disc7viqos7ht9a99k918saf8hgscq",
   userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "us-east-1_4ZTGQMA7B",
   region: process.env.NEXT_PUBLIC_COGNITO_REGION || "us-east-1",
-  redirectSignIn: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI || "http://localhost:3000/auth/callback",
-  redirectSignOut: process.env.NEXT_PUBLIC_COGNITO_SIGN_OUT_URI || "http://localhost:3000",
+  redirectSignIn: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI || "/auth/callback",
+  redirectSignOut: process.env.NEXT_PUBLIC_COGNITO_SIGN_OUT_URI || "/api/auth/logout",
   domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN || "d84l1y8p4kdic",
 }
 
